7105501e62fd9e1cdd1748aabc8708776007be85
mhmdsbr <saaber.mohamad@gmail.com>
2023-12-23 22:14:55 +0330

    typescript type error fix

--- src/components/WriteContract.tsx
+++ src/components/WriteContract.tsx
@@ -105,7 +105,7 @@ export function WriteContract() {
                 <Col>
                     {isSuccess && (
                         <>
-                            <SendTransaction mintedAmount={amountToMint} />
+                            <SendTransaction mintedAmount={parseFloat(amountToMint)} />
                         </>
                     )}
                 </Col>
