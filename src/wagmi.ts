879417cc377279479d20a2681872663846ec6c09
mhmdsbr <saaber.mohamad@gmail.com>
2023-12-24 12:25:49 +0330

    goerli on prod fix

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,24 +1,24 @@
 import { configureChains, createConfig } from 'wagmi';
-import { goerli, mainnet } from 'wagmi/chains';
+import { goerli} from 'wagmi/chains';
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
 import { publicProvider } from 'wagmi/providers/public';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 
-const alchemyApiKey = process.env.ALCHEMY_API_KEY;
+const alchemyApiKey = process.env.ALCHEMY_API_KEY || 'defaultApiKey';
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-    [mainnet, ...(process.env.NODE_ENV === 'development' ? [goerli] : [])],
+    [goerli],
     [
-      alchemyProvider({ apiKey: alchemyApiKey }),
-      publicProvider(),
+        alchemyProvider({ apiKey: alchemyApiKey }),
+        publicProvider(),
     ],
 );
 
 export const config = createConfig({
-  autoConnect: true,
-  connectors: [
-    new MetaMaskConnector({ chains }),
-  ],
-  publicClient,
-  webSocketPublicClient,
+    autoConnect: true,
+    connectors: [
+        new MetaMaskConnector({ chains }),
+    ],
+    publicClient,
+    webSocketPublicClient,
 });
